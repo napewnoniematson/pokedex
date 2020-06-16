d8b46ae40a38eb37fe8683ff13f488b83433907f
napewnoniematson <napewnoniematson@gmail.com>
2020-06-16 14:01:06 +0200

    Add WelcomeActivity.kt and set it as main activity

---
+++ app/src/main/java/com/example/pokedex/view/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pokedex.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pokedex.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
