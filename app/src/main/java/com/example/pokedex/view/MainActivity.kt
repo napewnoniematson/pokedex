642873cec784bf568705b1f7a47e183fe52aec4d
napewnoniematson <napewnoniematson@gmail.com>
2020-06-16 12:47:35 +0200

    MVVM structure

--- app/src/main/java/com/example/pokedex/view/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.pokedex
+package com.example.pokedex.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokedex.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
