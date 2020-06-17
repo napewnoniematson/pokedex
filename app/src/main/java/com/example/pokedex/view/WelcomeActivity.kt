c33868cbb89fdcc40931ac4bbe8a2a7b989c5dbb
napewnoniematson <napewnoniematson@gmail.com>
2020-06-17 11:48:04 +0200

    Downloading data messages (if success and if no network connection)

--- app/src/main/java/com/example/pokedex/view/WelcomeActivity.kt
+++ app/src/main/java/com/example/pokedex/view/WelcomeActivity.kt
@@ -2,11 +2,25 @@ package com.example.pokedex.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.pokedex.NetworkConnection
 import com.example.pokedex.R
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        if (NetworkConnection.isConnected(applicationContext)) {
+            // download data
+            clickToContinueTextView.text = getString(R.string.click_to_continue_text)
+        } else {
+            Toast.makeText(
+                applicationContext,
+                getString(R.string.no_network_connection_message_text),
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
