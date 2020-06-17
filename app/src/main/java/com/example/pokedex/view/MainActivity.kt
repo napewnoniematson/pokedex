b277f121049f35aa30d7df97ca2d7168772cb573
napewnoniematson <napewnoniematson@gmail.com>
2020-06-17 12:38:27 +0200

    preparing pokemonRecyclerView

--- app/src/main/java/com/example/pokedex/view/MainActivity.kt
+++ app/src/main/java/com/example/pokedex/view/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.pokedex.view
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import android.os.Bundle
 import com.example.pokedex.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        pokemonRecyclerView.layoutManager = LinearLayoutManager(this)
+//        pokemonRecyclerView.adapter = PokemonRVAdapter()
     }
 }
