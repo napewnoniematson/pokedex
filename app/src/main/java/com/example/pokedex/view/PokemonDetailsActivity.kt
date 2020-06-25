68cf0fd7a5c7bacdda0c84786c5d3934944aa29a
napewnoniematson <napewnoniematson@gmail.com>
2020-06-25 15:59:52 +0200

    First version of pokemon details layout

---
+++ app/src/main/java/com/example/pokedex/view/PokemonDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pokedex.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pokedex.R
+
+class PokemonDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pokemon_details)
+    }
+}
