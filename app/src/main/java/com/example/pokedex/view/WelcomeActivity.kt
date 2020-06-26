59bea1dd179c7b6f375ff2abe813a883a9cfa587
napewnoniematson <napewnoniematson@gmail.com>
2020-06-26 12:22:49 +0200

    dynamically changing welcome background

--- app/src/main/java/com/example/pokedex/view/WelcomeActivity.kt
+++ app/src/main/java/com/example/pokedex/view/WelcomeActivity.kt
@@ -8,11 +8,20 @@ import com.example.pokedex.NetworkConnection
 import com.example.pokedex.R
 import com.example.pokedex.view.animations.ZoomCloseFarAnimator
 import kotlinx.android.synthetic.main.activity_welcome.*
+import kotlin.random.Random
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        val backgroundImageId = when (Random.nextInt(3)) {
+            0 -> R.drawable.welcome_background_0
+            1 -> R.drawable.welcome_background_1
+            2 -> R.drawable.welcome_background_2
+            else -> R.drawable.welcome_background_0
+        }
+        welcome_layout.background = getDrawable(backgroundImageId)
 
         if (NetworkConnection.isConnected(applicationContext)) {
             // download data
